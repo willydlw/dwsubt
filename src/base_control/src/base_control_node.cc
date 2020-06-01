900100845ae7aa0baaa4bb01d9f63ac3f1e04997
diane <diane.williams@ucdenver.edu>
2020-06-01 09:49:52 -0600

    moves to entrance using state machine

--- src/base_control/src/base_control_node.cc
+++ src/base_control/src/base_control_node.cc
@@ -17,9 +17,18 @@
 
 #include <chrono>
 
+#include <geometry_msgs/Twist.h>
+
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <rosgraph_msgs/Clock.h>
+
+#include <std_srvs/SetBool.h>
+#include <subt_msgs/PoseFromArtifact.h>
+
+#include <subt_communication_broker/subt_communication_client.h>
+#include <subt_ign/CommonTypes.hh>
+#include <subt_ign/protobuf/artifact.pb.h>
 
 #include <string>
 #include <thread>
@@ -38,14 +47,43 @@ class BaseController
    public: BaseController(const std::string &name);
 
 
+
+   /// \brief Initialization routine performed only at start up. Sends 
+   /// start signal to subt. Initializes comm client, publishers, subscribers
+   /// \return true when ros call /subt/start service is successful. 
+   /// Otherwise returns false when calling service fails.
+   public: bool StartUp();
+
+   /// \brief Function to be called at start of robot's run to move from
+   /// start position to entrance of tunnel, cave, urban.
+   private: void MoveToEntrance(void);
+
+
    public: void Update();
 
 
    /// \brief ROS node handler
    private: ros::NodeHandle nh;
 
+
+   /// \brief publisher to send cmd_vel
+   private: ros::Publisher velPub;
+
+   /// \brief Communication client.
+   private: std::unique_ptr<subt::CommsClient> client;
+
+   /// \brief Client to request pose from origin.
+   private: ros::ServiceClient originClient;
+
+   /// \brief Service to request pose from origin.
+   private: subt_msgs::PoseFromArtifact originSrv;
+
    /// \brief mission state
    private: MissionState missionState;
+
+
+   /// \brief True if robot has arrived at destination
+   private: bool arrived{false};
 
    /// \brief robot name
    private: std::string rname;
@@ -60,6 +98,9 @@ BaseController::BaseController(const std::string &name)
    ros::topic::waitForMessage<rosgraph_msgs::Clock>("/clock", this->nh);
 
    // wait for the start service to be ready
+   ros::service::waitForService("/subt/start", -1);
+   ros::service::waitForService("/subt/pose_from_artifact_origin", -1);
+   ROS_WARN("remove wait for start when adapting for multiple robot launch");
 
 
    this->missionState = MissionState::AWAIT_START;
@@ -70,29 +111,209 @@ BaseController::BaseController(const std::string &name)
 }
 
 
+
+/////////////////////////////////////////////////
+bool BaseController::StartUp()
+{
+    // Send start signal
+    std_srvs::SetBool::Request req;
+    std_srvs::SetBool::Response res;
+    req.data = true;
+    if (!ros::service::call("/subt/start", req, res))
+    {
+      ROS_ERROR("Unable to send start signal.");
+      return false;
+    }
+    else
+    {
+      ROS_INFO("Sent start signal.");
+    }
+
+  
+    // Create subt communication client
+    //this->client.reset(new subt::CommsClient(this->name));
+    //this->client->Bind(&Controller::CommClientCallback, this);
+
+    // Create a cmd_vel publisher to control a vehicle.
+    this->velPub = this->nh.advertise<geometry_msgs::Twist>(
+        this->rname + "/cmd_vel", 1);
+
+    // Create a cmd_vel publisher to control a vehicle.
+    this->originClient = this->nh.serviceClient<subt_msgs::PoseFromArtifact>(
+        "/subt/pose_from_artifact_origin", true);
+    this->originSrv.request.robot_name.data = this->rname;
+   
+    return true;
+}
+
+
 void BaseController::Update()
 {
    switch(this->missionState)
    {
       case MissionState::AWAIT_START:
-         ROS_ERROR("BaseController::Update MissionState::AWAIT_START not completed");
+         ROS_DEBUG("changing state from AWAIT_START to INIT");
+         this->missionState = MissionState::INIT;
       break;
+
       case MissionState::INIT:
-         ROS_ERROR("BaseController::Update MissionState::INIT not completed");
+         if( this->StartUp())
+         {
+            ROS_INFO_STREAM("robot " << this->rname << " completed initialization");
+            this->missionState = MissionState::FIND_ENTRANCE;
+         }
+         else
+         {
+            ROS_ERROR("BaseController::Startup returned false");
+         }   
       break;
+
       case MissionState::FIND_ENTRANCE:
-         ROS_ERROR("BaseController::Update MissionState::AWAIT_START not completed");
+         if(this->arrived)
+         {
+            this->missionState = MissionState::STANDBY;
+            ROS_DEBUG("changing state from FIND_ENTRANCE to STANDBY");
+         }
+         else
+         {
+            ROS_DEBUG("still seeking the entrance");
+            this->MoveToEntrance();
+         }
       break;
+
       case MissionState::EXPLORE:
-         ROS_ERROR("BaseController::Update MissionState::AWAIT_START not completed");
+         ROS_ERROR("BaseController::Update MissionState::EXPLORE not completed");
       break;
       case MissionState::STANDBY:
-         ROS_ERROR("BaseController::Update MissionState::AWAIT_START not completed");
+         ROS_ERROR("BaseController::Update MissionState::STANDBY not completed");
       break;
       case MissionState::STOP:
-         ROS_ERROR("BaseController::Update MissionState::AWAIT_START not completed");
+         ROS_ERROR("BaseController::Update MissionState::STOP not completed");
       break;
    }
+}
+
+
+
+void BaseController::MoveToEntrance(void)
+{
+
+  static int arrivalCount = 0;
+  if (this->arrived){
+    if(arrivalCount == 0)
+    {
+      bool call = this->originClient.call(this->originSrv);
+      if (!call || !this->originSrv.response.success)
+      {
+        ROS_ERROR("Failed to call pose_from_artifact_origin service, \
+        robot may not exist, be outside staging area, or the service is \
+        not available.");
+      }
+      auto pose = this->originSrv.response.pose.pose;
+      ROS_INFO("Arrival position, x: %.2f, y: %.2f, z: %.2f", pose.position.x, pose.position.y, pose.position.z);
+      arrivalCount += 1;
+    }
+    
+    return;
+  }
+    
+
+  bool call = this->originClient.call(this->originSrv);
+  // Query current robot position w.r.t. entrance
+  if (!call || !this->originSrv.response.success)
+  {
+    ROS_ERROR("Failed to call pose_from_artifact_origin service, \
+robot may not exist, be outside staging area, or the service is \
+not available.");
+
+    // Stop robot
+    geometry_msgs::Twist msg;
+    this->velPub.publish(msg);
+    return;
+  }
+
+  auto pose = this->originSrv.response.pose.pose;
+
+  // Simple example for robot to go to entrance
+  geometry_msgs::Twist msg;
+
+  // Distance to goal
+  double dist = pose.position.x * pose.position.x +
+    pose.position.y * pose.position.y;
+
+  ROS_INFO("Distance to goal: %.2f", dist);
+  
+  // Arrived
+  if (dist < 0.3 || pose.position.x >= -0.3)
+  {
+    msg.linear.x = 0;
+    msg.angular.z = 0;
+    this->arrived = true;
+    ROS_INFO("Arrived at entrance!");
+    ROS_INFO("position: x %.2f, y %.2f, z %.2f", pose.position.x, pose.position.y, pose.position.z);
+  }
+  // Move towards entrance
+  else
+  {
+    // Yaw w.r.t. entrance
+    // Quaternion to yaw:
+    // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Source_Code_2
+    auto q = pose.orientation;
+    double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
+    double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+    auto yaw = atan2(siny_cosp, cosy_cosp);
+
+    ROS_INFO("yaw: %.2f", yaw);
+    ROS_INFO("position: x %.2f, y %.2f, z %.2f", pose.position.x, pose.position.y, pose.position.z);
+
+    auto facingFront = abs(yaw) < 0.1;
+    auto facingEast = abs(yaw + M_PI * 0.5) < 0.1;
+    auto facingWest = abs(yaw - M_PI * 0.5) < 0.1;
+
+    auto onCenter = abs(pose.position.y) <= 1.0;
+    auto westOfCenter = pose.position.y > 1.0;
+    auto eastOfCenter = pose.position.y < -1.0;
+
+    double linVel = 3.0;
+    double angVel = 1.5;
+
+    // Robot is facing entrance
+    if (facingFront && onCenter)
+    {
+      msg.linear.x = linVel;
+      msg.angular.z = angVel * -yaw;
+    }
+    // Turn to center line
+    else if (!facingEast && westOfCenter)
+    {
+      msg.angular.z = -angVel;
+    }
+    else if (!facingWest && eastOfCenter)
+    {
+      msg.angular.z = angVel;
+    }
+    // Go to center line
+    else if (facingEast && westOfCenter)
+    {
+      msg.linear.x = linVel;
+    }
+    else if (facingWest && eastOfCenter)
+    {
+      msg.linear.x = linVel;
+    }
+    // Center line, not facing entrance
+    else if (onCenter && !facingFront)
+    {
+      msg.angular.z = angVel * -yaw;
+    }
+    else
+    {
+      ROS_ERROR("Unhandled case");
+    }
+  }
+
+  this->velPub.publish(msg);
+
 }
 
 
